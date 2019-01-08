015c4d062aff258b30fa633d2c6b66f816cfc579
rlaqltmxm <rlaqltmxm@gmail.com>
2019-01-08 16:59:41 +0900

    .

--- app/src/main/java/io/madcamp/jh/madcamp_assignment2/SettingActivity.java
+++ app/src/main/java/io/madcamp/jh/madcamp_assignment2/SettingActivity.java
@@ -1,9 +1,12 @@
 package io.madcamp.jh.madcamp_assignment2;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -27,6 +30,43 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>(){
+            @Override
+            public void onSuccess(LoginResult loginResult){
+                Log.d("Test@FB", "onSucc");
+                LoginHelper.removeDialog();
+                LoginHelper.httpPostRegistered(SettingActivity.this);
+            }
+
+            @Override
+            public void onCancel() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingActivity.this);
+                builder.setMessage("Facebook에 로그인을 하시지 않으시면 이 앱의 대부분의 기능을 이용할 수 없습니다.");
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) { dialog.dismiss(); }
+                });
+                builder.create().show();
+                Log.d("Test@FB", "onCancel");
+                LoginHelper.removeDialog();
+                Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingActivity.this);
+                builder.setMessage("Facebook Error");
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) { dialog.dismiss(); }
+                });
+                builder.create().show();
+                Log.d("Test@FB", "onErr");
+                LoginHelper.removeDialog();
+            }
+        });
     }
 
     @Override
